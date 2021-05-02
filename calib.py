1fbd0783e5a3fcd9dc6af6b97ff6bc630a1c75f0
kim-jason <16kimsi@gmail.com>
2021-05-02 16:02:06 -0400

    Got all video frames

--- calib.py
+++ calib.py
@@ -1,13 +1,16 @@
 import cv2
 
-vid = cv2.VideoCapture('./labeled/0.hevc')
-ret, frame = vid.read()
-counter = 0
-while ret:
-    cv2.imwrite('./0.hevc_frame{counter}.jpg'.format(counter = counter), frame)
-    ret, frame = vid.read()
-    counter += 1
+num_vids = 5 # Harded coded in
 
+for num_vid in range(0, num_vids):
+    vid = cv2.VideoCapture('./labeled/{i}.hevc'.format(i = num_vid))
+    ret, frame = vid.read()
+    num_frames = 0
+
+    while ret:
+        cv2.imwrite('./labeled/{i}/{i}.hevc_frame{num_frames}.jpg'.format(i = num_vid, num_frames = num_frames), frame)
+        ret, frame = vid.read()
+        num_frames += 1
+    print(num_frames)
+    vid.release()
 print("Finished")
-print(counter)
-vid.release()
