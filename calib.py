c4cec313651a260e197487568671935cdc74b72e
kim-jason <16kimsi@gmail.com>
2021-05-04 01:09:56 -0400

    pitch and yaw values obtained

--- calib.py
+++ calib.py
@@ -2,6 +2,7 @@ import cv2
 import glob
 import tensorflow as tf
 import numpy as np
+import itertools
 
 #####CODE: SAVE ALL VIDEO FRAMES AS .JPG FOR THE FIVE LABELED VIDEOS#####
 # num_vids = 5 # Harded coded in
@@ -19,27 +20,41 @@ import numpy as np
 # print("Finished")
 
 
-#####CODE: Create all the tensors from the frames#####
-images = {}
+#####CODE: Create all the input arrays from the frames#####
+# images = {}
 
-for num_image in range(5):
-    image = glob.glob("./labeled/{num_image}/*.jpg".format(num_image = num_image))
-    images[num_image] = []
+# for num_image in range(5):
+#     image = glob.glob("./labeled/{num_image}/*.jpg".format(num_image = num_image))
+#     images[num_image] = []
 
-    for num_frame in range(len(image)):
-        frame = cv2.imread(image[num_frame])
-        frame = frame.reshape(-1).astype('float64')
-        frame /= 255.0
-        images[num_image].append(frame)
+#     for num_frame in range(len(image)):
+#         frame = cv2.imread(image[num_frame])
+#         frame = frame.reshape(-1).astype('float64')
+#         frame /= 255.0
+#         images[num_image].append(frame)
 
-frames = list(images.values())
-print(frames)
+# frames = list(itertools.chain.from_iterable(list(images.values())))
 
-# lines = []
-# with open('./labeled/0.txt') as f:
-#     lines = f.readlines()
 
-# print(len(lines))
-# print(lines[0])
+#####CODE: Create all the output values for each frame in the same order#####
+values = []
+for i in range(5):
+    with open('./labeled/{i}.txt'.format(i = i)) as f:
+        lines = f.readlines()
+        values.append(lines)
+values = list(itertools.chain.from_iterable(values))
+
+pitch_values = []
+yaw_values = []
+
+for value in values:
+    pitch, yaw = value.split(' ')
+    pitch = float(pitch)
+    yaw = float(yaw)
+    
+    pitch_values.append(pitch)
+    yaw_values.append(yaw)
+
+
 
 print("FINISHED")
